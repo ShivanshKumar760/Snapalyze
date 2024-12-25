e2fbd341a1472368fc32e7788338350b1809133f
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-12-25 14:33:00 +0530

    Snapalyze -gpt model integrated and front-end styled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,57 @@ import { useState } from 'react'
 
 function App() {
   const [image, setImage] = useState(null);
+  const [value,setValue]=useState("");
+  const [response,setResponse]=useState("");
+  const [error,setError]=useState("");
+  const [loading,setLoading]=useState(false);
 
+  const surpriseOptions=[
+    "Does this image has a cat",
+    "Is this image background red ",
+    "Does this image has puppies"
+  ];
+
+  const surprise=()=>{
+    const randomValue=surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue);
+
+  };
+
+
+  const analyzeImage=async()=>{
+    setLoading(true);
+    setResponse("");
+    if(!image)
+    {
+      setError("Error ! Must have an existing image!");
+      //early return
+      return;
+    }
+
+    try {
+      const options={
+        method:"POST",
+        body:JSON.stringify({message:value}),
+        headers:{"Content-type":"application/json"}
+      };
+      const response=await fetch("http://localhost:3000/openai",options);
+      const answer=await response.text();
+      setResponse(answer);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setError("Something didn't work pls try again");
+    }
+  
+  }
+
+  const clear=()=>{
+    setImage(null);
+    setValue("");
+    setResponse("");
+    setError("");
+  }
   const uploadImage=async (e)=>{
     console.log(e.target.files);
     const formData=new FormData();
@@ -22,15 +72,61 @@ function App() {
       console.log(data);
     } catch (error) {
       console.log(error);
+      setError("Somthing didn't work pls try again!");
     }
   }
   return (
     <>
-    <div className=''>
-      {image && <img className="uploadImageSection" src={URL.createObjectURL(image)} />}
+    <div className='app'>
+      <h1>Snapalyze</h1>
+      <section className='search-section'>
+          <div className="image-container">
+            {image && <img className="uploadImageSection" src={URL.createObjectURL(image)} />}
+          </div>
+          <p className="extra-info">
+            <span>
+              <label htmlFor="files" className="upload-label">Upload an Image</label>
+              <br />
+              <input 
+                className="uploadButton" 
+                onChange={uploadImage} 
+                type="file" 
+                id="files" 
+                accept="image/*" 
+              />
+            </span>
+          </p>
+
+
+          <p className='user-info-container'>
+            <span className='user-info'>What do you want to know about the image ?</span>
+            <button className='surprise' onClick={surprise} disabled={response}>Surprise me</button>
+          </p>
+          <br/>
+          <br/>
+          <div className='askME'>
+          <div className="input-container input__container">
+            <div class="shadow__input"></div>
+              <button class="input__button__shadow">
+              <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" height="20px" width="20px">
+                <path d="M4 9a5 5 0 1110 0A5 5 0 014 9zm5-7a7 7 0 104.2 12.6.999.999 0 00.093.107l3 3a1 1 0 001.414-1.414l-3-3a.999.999 0 00-.107-.093A7 7 0 009 2z" fill-rule="evenodd" fill="#17202A"></path>
+              </svg>
+              </button>
+            <input value={value} placeholder="What's in the image..." onChange={(e)=>{setValue(e.target.value)}} class="input__search"/>
+          </div>
+          {(!response && !error)&&<button onClick={analyzeImage}>Ask Me</button>}
+
+          {(response || error)&&<button onClick={clear}>Clear</button>}
+
+          </div>
+         
+
+          {error && <p>{error}</p>}
+          {loading ? "Loading":response && <p className='answer'>{response}</p>}
+      </section>
+
       <br/>
-      <label htmlFor='files'>upload and image</label>
-      <input onChange={uploadImage} type='file' id="files" accept='image/*'/>
+
     </div>
     </>
   )
--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
-// import OpenAI from "openai";
+import OpenAI from "openai";
 import dotenv from "dotenv";
 import fs from "fs";
 import multer from "multer";
 dotenv.config();
 
 const app=express();
+const openai=new OpenAI({apiKey:process.env.OPENAI_KEY});
 const port=process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
@@ -32,6 +33,35 @@ app.post("/upload",(req,res)=>{
         }
         filePath=req.file.path;
     });
+});
+
+
+app.post("/openai",async (req,res)=>{
+    try {
+        const prompt=req.body.message;
+        console.log("User Prompt is:",prompt);
+        const imageAsBase6=fs.readFileSync(filePath,"base64");
+        const response = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages:[
+                {
+                 role:"user",
+                 content:[
+                    {type:"text",text:prompt},
+                    {type:"image_url",image_url:{url:`data:image/jpeg;base64,${imageAsBase6}`}
+                }
+            ]
+    
+                }
+            
+            ]
+        });
+    
+        console.log(response.choices[0].message);
+        res.send(response.choices[0].message.content);
+    } catch (error) {
+       console.log(error); 
+    }
 });
 
 
